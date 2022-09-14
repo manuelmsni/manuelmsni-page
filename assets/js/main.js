cbbff78850322c3aabc527da0fd00c619beb1d3c
manuelmsni <manuelmsni@hotmail.com>
2022-09-14 02:59:03 +0200

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -15,3 +15,29 @@ $(window).scroll(function() {
     $(`.${ShowClass}`).fadeIn('slow');
     $(`.${HideClass}`).hide();
   }
+
+window.onload = () => {
+	let rgbText, nodes, hues;
+	
+	rgbText = document.querySelector(".rgb");
+
+	rgbText.innerHTML = [].slice
+		.call(rgbText.innerHTML)
+		.map(c => `<span>${c}</span>`)
+		.join("");
+
+	nodes = document.querySelectorAll(".rgb span");
+	hues = [];
+
+	nodes.forEach((c, i) => {
+		hues.push(Math.round(i * (360 / nodes.length)));
+	});
+	
+	(function loop() {
+		hues.forEach((h, i, _this) => {
+			_this[i]--;
+			nodes[i].style.color = `hsl(${_this[i]},100%,50%)`;
+		});
+		window.requestAnimationFrame(loop);
+	})();
+};
