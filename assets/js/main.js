4d7f7d6303c58316410a6be40e4091a640390a8d
manuelmsni <manuelmsni@hotmail.com>
2022-09-15 14:10:58 +0000

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -50,7 +50,9 @@ function showColorTable() {
 }
 
 $( document ).ready( function() {
+	function date() {
 	var now = new Date();
 	var time = now.getHours() + ":" + now.getMinutes() + ":" + now.getSeconds();
 	document.getElementById('date').innerHTML = time;
+	}
 } )
