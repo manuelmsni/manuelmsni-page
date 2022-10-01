56fe6dc629086e846420ef19230f2f32e9d7338d
manuelmsni <manuelmsni@hotmail.com>
2022-10-01 18:26:35 +0200

    Add files via upload

--- assets/js/main.js
+++ assets/js/main.js
@@ -17,33 +17,8 @@ $(window).scroll(function() {
   }
 
 window.onload = () => {
-
 	$('#header').load('manuelmsni.com/assets/bits/header.html');
 	$('#footer').load('manuelmsni.com/assets/bits/footer.html');
-
-	let rgbText, nodes, hues;
-	
-	rgbText = document.querySelector(".rgb");
-
-	rgbText.innerHTML = [].slice
-		.call(rgbText.innerHTML)
-		.map(c => `<span>${c}</span>`)
-		.join("");
-
-	nodes = document.querySelectorAll(".rgb span");
-	hues = [];
-
-	nodes.forEach((c, i) => {
-		hues.push(Math.round(i * (360 / nodes.length)));
-	});
-	
-	(function loop() {
-		hues.forEach((h, i, _this) => {
-			_this[i]--;
-			nodes[i].style.color = `hsl(${_this[i]},100%,50%)`;
-		});
-		window.requestAnimationFrame(loop);
-	})();
 };
 
 
