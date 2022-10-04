21503b07788a7c3733ed0e482c14e322d7f26f55
manuelmsni <manuelmsni@hotmail.com>
2022-10-04 16:22:41 +0200

    Add files via upload

---
+++ assets/js/main.js
@@ -0,0 +1,26 @@
+// Toggle .header-scrolled class to #header when page is scrolled
+$(window).scroll(function() {
+    if ($(this).scrollTop() > 50) {
+      $('#header').addClass('header-scrolled');
+    } else {
+      $('#header').removeClass('header-scrolled');
+    }
+  });
+
+  if ($(window).scrollTop() > 50) {
+    $('#header').addClass('header-scrolled');
+  }
+
+  function ShowHide(ShowClass, HideClass) {
+    $(`.${ShowClass}`).fadeIn('slow');
+    $(`.${HideClass}`).hide();
+  }
+
+window.onload = () => {
+	$('#header').load('../../../../../../assets/bits/header.html');
+	$('#footer').load('../../../../../../bits/footer.html');
+};
+
+function loadHTML(elementId, path) {
+	$(`#${elementId}`).load(`${path}`);
+}
